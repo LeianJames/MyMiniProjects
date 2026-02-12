df738d0cc40507aa0662651930decbea45e4594b
Leian <leijames28@gmail.com>
2026-02-13 06:32:12 +0800

    Resolved conflicts

--- multiplicationtable.py
+++ multiplicationtable.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #This function shows the multiplication table of numbers 1 through 10
 
 def table():
@@ -7,14 +6,4 @@ def table():
             k = i * j
             print(f"{i} x {j} = {k} \n" )
 
-=======
-#This function shows the multiplication table of numbers 1 through 10
-
-def table():
-    for i in range (1, 11):
-        for j in range(1,11):
-            k = i * j
-            print(f"{i} x {j} = {k} \n" )
-
->>>>>>> 8d3122fedf6a1a153ba05f51605d207c37bda853
 table()
