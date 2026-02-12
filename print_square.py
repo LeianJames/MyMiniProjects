472a717e2c95abbc6afadc839a8808e923ef7db0
Leian <leijames28@gmail.com>
2026-02-13 06:30:10 +0800

    Initial commit with new files

--- print_square.py
+++ print_square.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #This program prints out squares 
 # size = 3 -->  ###
 #               ###
@@ -15,17 +14,4 @@ def print_row(size):
 def print_square(size):
         print("#" * size)
 
-=======
-def main():
-    size = int(input("What's the size of the box? "))
-    print_row(size)
-
-def print_row(size):
-    for i in range(size):
-        print_square(size)
-
-def print_square(size):
-        print("#" * size)
-
->>>>>>> 8d3122fedf6a1a153ba05f51605d207c37bda853
 main()
